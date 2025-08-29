f588823aba745ad64ce7a9860ca98721bc535b22
Ethan Resnick <ethan.resnick@gmail.com>
2025-08-29 15:24:46 -0400

    docs: explain DrainOuterGeneric

--- types/simplify.d.ts
+++ types/simplify.d.ts
@@ -1,3 +1,12 @@
+/**
+ * Apply this to a type that's NOT a conditional type (like a mapped type or an
+ * indexed access type) to prevent the computations required to resolve that
+ * type from contributing to Typescript's internal type instantiation depth
+ * counter, thereby preventing many "Type instantiation is excessively deep and
+ * possibly infinite" errors.
+ *
+ * See Kysely type of the same name for more details.
+ */
 export type DrainOuterGeneric<T> = [T] extends [unknown] ? T : never;
 
 export type Simplify<T> = DrainOuterGeneric<{ [K in keyof T]: T[K] } & {}>;
